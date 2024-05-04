4b77f65ae2623d06623e8954aafe5c4f926aa060
arshadhs <arshadhs@gmail.com>
2024-05-04 17:05:00 +0100

    boost::shared_ptr

--- smartPointersRAII/boost_shared_Pointer.cpp
+++ smartPointersRAII/boost_shared_Pointer.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
-#include<boost\_shared_Pointer.cpp>
-//#include <C:\study\boost_1_39_0\boost\smart_ptr\shared_ptr.hpp>
-
-//ToDo: Fix Compilation issue
+#include<boost\smart_ptr\shared_ptr.hpp>
+#include<boost\smart_ptr\scoped_ptr.hpp>
+#include<boost\smart_ptr\make_shared.hpp>
 
 int main ()
 {
-
 	int i = 4;
 	float j = 4.5;
 	int k = 5;
 
-	boost::shared_ptr <int> p (new int);
+	//boost::shared_ptr <int> p (new int);
+
+	boost::shared_ptr <int> p = boost::make_shared<int>();
 	boost::shared_ptr <void> pV(p);
+
+	//	boost::scoped_ptr <void> pSp(p);
 
 	std::cout << i << std::endl;
 	std::cout << j << std::endl;
 	std::cout << *p << std::endl;
 
 	p.reset(new int(k));
+
 	std::cout << *p << std::endl;
 
 	pV = p;
