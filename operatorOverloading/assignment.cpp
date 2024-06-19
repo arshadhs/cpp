bd6ca271b6015797c5c77b1eeccac257501ef37e
arshadhs <arshadhs@gmail.com>
2024-06-19 22:32:22 +0100

    operator overloading

---
+++ operatorOverloading/assignment.cpp
@@ -0,0 +1,8 @@
+///     https://www.geeksforgeeks.org/cpp-assignment-operator-overloading
+///
+///     ClassName& operator= (const ClassName& other);
+///
+/// Assignment Operators are predefined to operate only on built-in Data types.
+/// 1) Assignment operator overloading is binary operator overloading.
+/// 2) Overloading assignment operator in C++ copies all values of one object to another object.
+/// 3) Only a non-static member function should be used to overload the assignment operator.
